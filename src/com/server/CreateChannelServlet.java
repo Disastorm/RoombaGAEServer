24fd8c8f38ba4f6e7ddd771e59d25285a19c6486
U-Peter-THINK\Peter <Peter@Peter-THINK.(none)>
2014-10-02 17:48:45 -0700

    channel

---
+++ src/com/server/CreateChannelServlet.java
@@ -0,0 +1,40 @@
+package com.server;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.client.GreetingService;
+import com.google.appengine.api.channel.ChannelService;
+import com.google.appengine.api.channel.ChannelServiceFactory;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.shared.FieldVerifier;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class CreateChannelServlet extends HttpServlet{
+	
+	private static ChannelService channelService = ChannelServiceFactory.getChannelService();
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String channelKey = req.getParameter("c");
+
+		// Create a Channel using the 'channelKey' we received from the client
+		String token = channelService.createChannel(channelKey);
+
+		// Send the client the 'token' + the 'channelKey' this way the client
+		// can start using the new channel
+		resp.setContentType("application/json");
+		StringBuffer sb = new StringBuffer();
+		sb.append("{\"channelKey\":\"");
+		sb.append(channelKey);
+		sb.append("\",\"token\":\"");
+		sb.append(token);
+		sb.append("\"}");
+		resp.getWriter().write(sb.toString());
+	}
+}
